4a8d2f038c43d1a99f90848c68e5ea935a256fcb
Yu <Yu>
2019-01-20 11:17:11 +0900

    Fix source code(Checkstyle)

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -21,8 +21,8 @@ public class SampleDTO implements Serializable {
 
     /**
      * Store the user name.
-     * @param firstName first name of the user
-     * @param lastName last name of the user
+     * @param frstName first name of the user
+     * @param lstName last name of the user
      */
     public SampleDTO(final String frstName, final String lstName) {
         this.setFirstName(frstName);
@@ -56,7 +56,7 @@ public class SampleDTO implements Serializable {
 
     /**
      * Set first name of the user.
-     * @param firstName first name of the user
+     * @param frstName first name of the user
      */
     public final void setFirstName(final String frstName) {
         this.firstName = frstName;
@@ -64,7 +64,7 @@ public class SampleDTO implements Serializable {
 
     /**
      * Set last name of the user.
-     * @param lastName last name of the user
+     * @param lstName last name of the user
      */
     public final void setLastName(final String lstName) {
         this.lastName = lstName;
